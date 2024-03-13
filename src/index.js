c10c0377df4c77aa96ddcbc31b59933f29a34792
TheoAdal <theoadal@gmail.com>
2024-03-13 22:23:01 +0200

    blah blah

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
@@ -6,15 +5,6 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
->>>>>>> 9cfd167b871717774231c61a86c0717bc29e86fc
 root.render(
   <React.StrictMode>
     <App />
