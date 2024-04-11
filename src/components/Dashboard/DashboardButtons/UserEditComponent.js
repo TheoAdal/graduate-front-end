5167ba423a5c8d7a3ecb82f521f3f311b8063c24
TheoAdal <theoadal@gmail.com>
2024-04-11 23:32:04 +0300

    nav side and footer

--- src/components/Dashboard/DashboardButtons/UserEditComponent.js
+++ src/components/Dashboard/DashboardButtons/UserEditComponent.js
@@ -105,75 +105,10 @@ const UserEditComponent = () => {
 
   return (
     <div className="sb-nav-fixed">
-      <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        {/* Navbar Brand */}
-        <a className="navbar-brand ps-3" href="#">
-          Admin Dashboard
-        </a>
-        {/* Sidebar Toggle */}
-        <button
-          className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
-          id="sidebarToggle"
-          href="#"
-        >
-          <i className="fas fa-bars"></i>
-        </button>
-        {/* Navbar */}
-        <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-          <NavDropdown title="Options" id="basic-nav-dropdown">
-            <NavDropdown.Item>
-              <Link to="/profile">Profile</Link>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <Link to="/login">Logout</Link>
-            </NavDropdown.Item>
-          </NavDropdown>
-        </ul>
-      </nav>
+      <TopNav userRole={userRole}/>
       <div id="layoutSidenav">
         <div id="layoutSidenav_nav">
-          <nav
-            className="sb-sidenav accordion sb-sidenav-dark"
-            id="sidenavAccordion"
-          >
-            <div className="sb-sidenav-menu">
-              <div className="nav">
-                <div className="sb-sidenav-menu-heading">Addons</div>
-                {/* Change href */}
-                <a className="nav-link" href="/volunteerlist">
-                  <div className="sb-nav-link-icon">
-                    <i className="fas fa-chart-area"></i>
-                  </div>
-                  Volunteer List
-                </a>
-                {/* Change href */}
-                <a className="nav-link" href="/olduserlist">
-                  <div className="sb-nav-link-icon">
-                    <i className="fas fa-table"></i>
-                  </div>
-                  Old User List
-                </a>
-                {/* Change href */}
-                <a className="nav-link" href="/employeelist">
-                  <div className="sb-nav-link-icon">
-                    <i className="fas fa-table"></i>
-                  </div>
-                  Manager List
-                </a>
-                {/* Change href */}
-                <a className="nav-link" href="/calendar">
-                  <div className="sb-nav-link-icon">
-                    <i className="fas fa-table"></i>
-                  </div>
-                  Calendar
-                </a>
-              </div>
-            </div>
-            <div className="sb-sidenav-footer">
-              <div className="small">Logged in as:</div>
-              Admin
-            </div>
-          </nav>
+        <Sidebar userRole={userRole} userName={userName} userSurname={userSurname} />
         </div>
         <div id="layoutSidenav_content">
           <div class="container-fluid px-4">
@@ -376,20 +311,7 @@ const UserEditComponent = () => {
               </div>
             </div>
           </div>
-          <footer className="py-4 bg-light mt-auto">
-            <div className="container-fluid px-4">
-              <div className="d-flex align-items-center justify-content-between small">
-                <div className="text-muted">
-                  Copyright &copy; Your Website 2025
-                </div>
-                <div>
-                  <a href="#">Privacy Policy</a>
-                  &middot;
-                  <a href="#">Terms &amp; Conditions</a>
-                </div>
-              </div>
-            </div>
-          </footer>
+          <Footer />
         </div>
       </div>
     </div>
