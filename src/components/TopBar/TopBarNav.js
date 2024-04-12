cbbde3b03b0649573a6577a74932fb664a3ffbdc
TheoAdal <theoadal@gmail.com>
2024-04-12 16:49:44 +0300

    fixed navbar buttons for when a user is logged in and added Homepage button for dashboard TopNav

--- src/components/TopBar/TopBarNav.js
+++ src/components/TopBar/TopBarNav.js
@@ -1,5 +1,5 @@
 import "./TopBarNavStyles.scss";
-import React from "react";
+import React, { useContext} from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,40 +7,34 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../../images/logo192.png";
 
-//#00A98F or #03957F #1B365D #193782 #A61E2F    #D47700 #24785E
+import { AuthContext } from "../Content/LoginPage/AuthContext";
+import LogoutButton from "../Dashboard/LogoutButton";
+
+//#00A98F or #03957F #1B365D #193782 #A61E2F  #D47700 #24785E
 function TopBarNav() {
-  // const userId = localStorage.getItem("userId");
-  // const userName = localStorage.getItem("userName");
-  // const userSurname = localStorage.getItem("userSurname");
-  // const userRole = localStorage.getItem("userRole");
+  const userId = localStorage.getItem("userId");
+  const userName = localStorage.getItem("userName");
+  const userSurname = localStorage.getItem("userSurname");
+  const userRole = localStorage.getItem("userRole");
 
-  // const navigate = useNavigate();
+  const { token } = useContext(AuthContext); // Access token from AuthContext
+  const navigate = useNavigate();
 
-  // const getDashboardRoute = () => {
-  //   if (userId) {
-  //     // Navigate to the dashboard based on the user's role
-  //     switch (userRole) {
-  //       case "admin":
-  //         navigate("/admindash");
-  //         break;
-  //       case "manager":
-  //         navigate("/managerdash");
-  //         break;
-  //       case "volunteer":
-  //         navigate("/volunteerdash");
-  //         break;
-  //       case "olduser":
-  //         navigate("/olduserdash");
-  //         break;
-  //       default:
-  //         navigate("/sjdvbfjsfvbuhsd");
-  //         break;
-  //     }
-  //   } else {
-  //     // Navigate to login if user is not logged in
-  //     navigate("/login");
-  //   }
-  // };
+  // Function to generate the dashboard link based on user's role
+  const getDashboardLink = () => {
+    switch (userRole) {
+      case "admin":
+        return "/admindash";
+      case "manager":
+        return "/managerdash";
+      case "volunteer":
+        return "/volunteerdash";
+      case "olduser":
+        return "/olduserdash";
+      default:
+        return "/";
+    }
+  };
 
   const navbarStyle = {
     backgroundColor: "#00A98F",
@@ -62,21 +56,20 @@ function TopBarNav() {
             <Link to="/">Home</Link>
             <Link to="/about">About us</Link>
             <Link to="/contact">Contact us</Link>
-            {/* {userId ? ( */}
-              {/*  <> */}
-                {/* <Link to={getDashboardRoute(userRole)}>Dashboard</Link> */}
-                {/* <Link onClick={handleDashboardClick}>Dashboard</Link> */}
-                {/* <NavDropdown title="Options" id="basic-nav-dropdown">
+            {token ? (
+              <>
+                <Link to={getDashboardLink()}>Dashboard</Link> 
+                <NavDropdown title="Options" id="basic-nav-dropdown">
                   <NavDropdown.Item>
                     <Link to="/profile">Profile</Link>
                   </NavDropdown.Item>
                   <NavDropdown.Item>
-                    <Link to="/logout">Logout</Link>
+                    <LogoutButton />  
                   </NavDropdown.Item>
                 </NavDropdown>
               </>
             ) : (
-              <> */}
+              <>
                 <Link to="/login">Log in</Link>
                 <NavDropdown title="Register as" id="basic-nav-dropdown">
                   <NavDropdown.Item>
@@ -86,8 +79,8 @@ function TopBarNav() {
                     <Link to="/olduser">Person in need</Link>
                   </NavDropdown.Item>
                 </NavDropdown>
-              {/* </> */}
-            {/* )} */}
+              </>
+            )}
           </Nav>
         </Container>
       </Navbar>
