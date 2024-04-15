bb616ec69dd1ea60aa7f1f1e07703fe6a6d8c145
TheoAdal <theoadal@gmail.com>
2024-04-15 23:22:06 +0300

    created Email verification NEEDS CHANGES

--- src/components/Dashboard/DashboardButtons/UserEditComponent.js
+++ src/components/Dashboard/DashboardButtons/UserEditComponent.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
