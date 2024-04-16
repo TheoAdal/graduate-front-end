ab9a957652d233bc3790b44725803aaa1c989e7e
TheoAdal <theoadal@gmail.com>
2024-04-16 18:28:18 +0300

    small cahnges.

--- src/components/Dashboard/DashboardNav/TopNav.js
+++ src/components/Dashboard/DashboardNav/TopNav.js
@@ -31,7 +31,7 @@ const TopNav = ({ handleLogout, userRole }) => {
       </a>
       <button
         className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
-        id="sidebarToggle"
+        id="sidebarToggle" //TOPBAR OPTIONS TOGGLE gia scss.
         href="#"
       >
         <i className="fas fa-bars"></i>
